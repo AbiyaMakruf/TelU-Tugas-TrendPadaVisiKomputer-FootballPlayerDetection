09f4192a0567ae27a4f772ac7e9dee9e720f7393
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-03-22 17:23:39 +0700

    edit: memperbaiki script run pm2

--- pm2.sh
+++ pm2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Setup pm2
+
+# Run Streamlit
 chmod +x bash/run_streamlit.sh
 pm2 start bash/run_streamlit.sh --name streamlit-app
 
+# Jeda 2 detik
+sleep 2
+
+# Run Flask
 chmod +x bash/run_flask.sh
 pm2 start bash/run_flask.sh --name backend-app
 
